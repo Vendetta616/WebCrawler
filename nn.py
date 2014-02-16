38a982f9d510fe10e1207d1f4bff05b2479b1efa
VENDETTA <randuinsomen@gmail.com>
2014-02-17 00:39:26 +0900

    add nn.py

---
+++ nn.py
@@ -0,0 +1,63 @@
+from math import tanh
+import sqlite3
+
+class searchnet:
+	def __init__(self,dbname):
+		self.con = sqlite3.connect(dbname)
+
+	def __del__(self):
+		self.con.close()
+
+	def maketables(self):
+		self.con.execute("create table hiddennode(create_key)")
+		self.con.execute("create table wordhidden(fromid,toid,strength)")
+		self.con.execute("create table hiddenurl(fromid,toid,strength)")
+		self.con.commit()
+
+	def getstrength(self,fromid,toid,layer):
+		if layer == 0:
+			table = "wordhidden"
+		else:
+			table = "hiddenurl"
+		res = self.con.execute("select strength from {} where fromid = {} and toid ={}".format(table,fromid,toid)).fetchone()
+
+		if res ==None:
+			if layer ==0:
+				return -0.2
+			if layer ==1:
+				return 0
+		return res[0]
+
+
+	def setstrength(self,fromid,toid,layer,strength):
+		if layer ==0:
+			table = "wordhidden"
+		else:
+			table ="hiddenurl"
+
+		res =self.con.execute("select rowid from {} where fromid = {} and toid ={}".format(table,fromid,toid)).fetchone()
+
+		if res ==None:
+			self.con.execute("insert into {} (fromid,toid,strength) values ({},{},{:f})".format(table,fromid,toid,strength))
+		else:
+			rowid = res[0]
+			self.con.execute("update {} set strength = {:f} where rowid = {}".format(table,strength,rowid))
+
+	def generatehiddennode(self,wordids,urls):
+		if len(wordids)>3:
+			return None
+
+		#check Node has already created with this word set
+		createkey = "_".join(sorted([str(wi) for wi in wordids]))
+		res = self.con.execute("select rowid from hiddennode where create_key = '{}'".format(createkey)).fetchone()
+
+		#if not exits node, create node
+		if res == None:
+			cur= self.con.execute("insert into hiddennode (create_key) values ('{}')".format(createkey))
+			hiddenid = cur.lastrowid
+			#set default value
+			for wordid in wordids:
+				self.setstrength(wordid,hiddenid,0,1.0/len(wordids))
+			for urlid in urls:
+				self.setstrength(hiddenid,urlid,1,0.1)
+			self.con.commit()
--- script.py
+++ script.py
@@ -18,5 +18,20 @@ def calc():
 	__doscript__crawler = crawler.crawler("searchindex.db")
 	__doscript__crawler.calculatepagerank()
 
+def nn():
+	import nn
+	mynet = nn.searchnet("nn.db")
+	mynet.maketables()
+	wWorld,wRiver,wBank = 101,102,103
+	uWorldBank,uRiver,uEarth = 201,202,203
+	mynet.generatehiddennode([wWorld,wBank],[uWorldBank,uRiver,uEarth])
+
+	for c in mynet.con.execute("select * from wordhidden"):
+		print(c)
+
+	for c in mynet.con.execute("select * from hiddenurl"):
+		print(c)
+
+
 def __init__(self):
 	doscript()
