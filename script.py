099400b8c65f9eaa9f1b1adc9826f08bc3ad0f1b
VENDETTA <randuinsomen@gmail.com>
2014-02-24 23:27:26 +0900

    fix nn.py and script.py

--- script.py
+++ script.py
@@ -40,35 +40,7 @@ def nn2():
 	mynet = nn.searchnet("nn.db")
 	print(mynet.getresult([wWorld,wBank],[uWorldBank,uRiver,uEarth]))
 
-def dtanh(y):
-	return 1.0-y*y
 
-def backpropagate(self,targets,N=0.5):
-	#calculate error of output
-	output_deltas = [0.0] *len(self.urlids)
-	for k in range(len(self.urlids)):
-		error = targets[k]-self.ao[k]
-		output_deltas[k] = dtanh(self.ao[k])*error
-
-	#calculate error of hiddenlayer
-	hidden_deltas = [0.0] * len(self.hiddenids)
-	for j in range(len(self.hiddenids)):
-		error = 0.0
-		for k in range(len(self.urlids)):
-			error = error + output_deltas[k]*self.wo[j][k]
-		hidden_deltas[j] = dtanh(self.ah[j])*error
-
-	#calculate weight of output
-	for j in range(len(self.hiddenids)):
-		for k in range(len(self.urlids)):
-			change = output_deltas[k]*self.ah[j]
-			self.wo[j][k] = self.wo[j][k] + N*change
-
-	#Update weight of input
-	for i in range(len(self.wordids)):
-		for j in range(len(self.hiddenids)):
-			change = hidden_deltas[j]*self.ai[i]
-			self.wi[i][j] = self[i][j]+N*change
 
 def __init__(self):
 	doscript()
